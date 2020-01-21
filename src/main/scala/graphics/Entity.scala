8eb5801f98d4265c8a6110c79af92cfb25006d4e
Caden Young <cyoung5@trinity.edu>
2020-01-21 10:56:37 -0600

    Same

--- src/main/scala/graphics/Entity.scala
+++ src/main/scala/graphics/Entity.scala
@@ -1,8 +1,8 @@
 package graphics
 
 class Entity(val x: Double, val y: Double) {
-    def width: Double = 1
-    def height: Double = 1
+    def width: Double = 1.0
+    def height: Double = 1.0
 
     def intersects(other: Entity): Boolean = {
         (x - other.x).abs < (width + other.width) / 2 && (y - other.y).abs < (height + other.height) / 2
